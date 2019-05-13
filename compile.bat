f5897bbd0c62aaaa5280c96a60ed21673034c65e
Saillou <chauvinremy@upyourprod.fr>
2019-05-13 10:38:00 +0900

    Automatic commit

--- compile.bat
+++ compile.bat
@@ -3,9 +3,10 @@ setlocal EnableDelayedExpansion
 
 cd /d %~dp0
 set fRelease=%~dp0Release\
+set launch=0
 
 call compileCode.bat Server mainServer
-call compileCode.bat Client mainClient
+REM call compileCode.bat Client mainClient
 
 :: Launch on success
 cd /d %fRelease%/
@@ -21,14 +22,26 @@ if not exist Client.exe (
 	echo Client build has failed.
 	set /a "bug=1"
 )
+
 if !bug!==1 (
 	echo. & pause & exit
+) else (
+	cd /d %~dp0
+	call git add *
+	call git add Sources/*
+	call git commit -m "Automatic commit"
+	call git push
+	cd /d %fRelease%/
+	echo.
+	pause
 )
 
 
 :: Launch
-start "Server" Server.exe
-timeout 1
-start "Client" Client.exe
-REM pause
+if %launch%==1 (
+	call "Server" Server.exe
+	REM timeout 1
+	REM start "Client" Client.exe
+)
+
 exit
--- compile.sh
+++ compile.sh
@@ -1,2 +1,3 @@
 git pull
 g++ -o test -std=gnu++11 Sources/mainServer.cpp -lpthread
+./test
