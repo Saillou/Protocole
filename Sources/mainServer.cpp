9f97a4d80c8bb74f6dee33f98aa3d78dff24dcc9
Saillou <chauvinremy@upyourprod.fr>
2019-05-13 10:19:30 +0900

    Automatic commit

--- Sources/mainServer.cpp
+++ Sources/mainServer.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <csignal>
 
-#include "Server.hpp"
-#include "Timer.hpp"
+// #include "Server.hpp"
+// #include "Timer.hpp"
 #include "Message.hpp"
 
 namespace Globals {
@@ -11,7 +11,7 @@ namespace Globals {
 	
 	// Variables
 	volatile std::sig_atomic_t signalStatus = 0;
-	std::atomic<bool> beginSend = false;
+	// std::atomic<bool> beginSend = false;
 }
 
 // --- Signals ---
@@ -25,50 +25,51 @@ int main() {
 	std::signal(SIGINT, sigintHandler);
 	
 	//-- Create server
-	Server server;
-	server.connectAt(Globals::PORT);
+	// Server server;
+	// server.connectAt(Globals::PORT);
 	
-	if(!server.isConnected())
-		return 1;
+	// if(!server.isConnected())
+		// return 1;
 	
-	// -------- Callbacks --------	
-	server.onClientConnect([&](const Server::ClientInfo& client) {
-		std::cout << "New client, client_" << client.id << std::endl;
-	});
-	server.onClientDisconnect([&](const Server::ClientInfo& client) {
-		std::cout << "Client quit, client_" << client.id << std::endl;
-	});
-	server.onError([&](const Error& error) {
-		std::cout << "Error : " << error.msg() << std::endl;
-	});
+	// // -------- Callbacks --------	
+	// server.onClientConnect([&](const Server::ClientInfo& client) {
+		// std::cout << "New client, client_" << client.id << std::endl;
+	// });
+	// server.onClientDisconnect([&](const Server::ClientInfo& client) {
+		// std::cout << "Client quit, client_" << client.id << std::endl;
+	// });
+	// server.onError([&](const Error& error) {
+		// std::cout << "Error : " << error.msg() << std::endl;
+	// });
 	
-	server.onInfo([&](const Server::ClientInfo& client, const Message& message) {
-		std::cout << "Info received from client_" << client.id << ": [Code:" << message.code() << "] " << message.str() << std::endl;
-		if(message.str() == "begin")
-			Globals::beginSend = true;
-	});
-	server.onData([&](const Server::ClientInfo& client, const Message& message) {
-		std::cout << "Data received from client_" << client.id << ": [Code:" << message.code() << "] " << message.str() << std::endl;
-	});
+	// server.onInfo([&](const Server::ClientInfo& client, const Message& message) {
+		// std::cout << "Info received from client_" << client.id << ": [Code:" << message.code() << "] " << message.str() << std::endl;
+		// if(message.str() == "begin")
+			// Globals::beginSend = true;
+	// });
+	// server.onData([&](const Server::ClientInfo& client, const Message& message) {
+		// std::cout << "Data received from client_" << client.id << ": [Code:" << message.code() << "] " << message.str() << std::endl;
+	// });
 	
 	
 	// -------- Main loop --------
 	Message bigMessage = "Hello world!";
+	std::cout << bigMessage.str() << std::endl;
 	
-	for(Timer timer; Globals::signalStatus != SIGINT; timer.wait(1)) {
-		// /* ... Do stuff ... */
-		static int i = 0;
-		if(Globals::beginSend && i <= 0) {
-			for(auto& client : server.getClients()) {
-				server.sendData(client, bigMessage);
-			}
-			i++;
-		}
-	}
+	// for(Timer timer; Globals::signalStatus != SIGINT; timer.wait(1)) {
+		// // /* ... Do stuff ... */
+		// static int i = 0;
+		// if(Globals::beginSend && i <= 0) {
+			// for(auto& client : server.getClients()) {
+				// server.sendData(client, bigMessage);
+			// }
+			// i++;
+		// }
+	// }
 		
-	// -- End
-	server.disconnect();
+	// // -- End
+	// server.disconnect();
 	
-	std::cout << "Clean exit" << std::endl;
+	// std::cout << "Clean exit" << std::endl;
 	return system("pause");
 }
