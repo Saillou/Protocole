34e27ecdd91cf0558346a88aee6448ec69d307f3
Saillou <chauvinremy@upyourprod.fr>
2019-05-23 19:14:22 +0900

    Automatic commit

--- Sources/mainClient.cpp
+++ Sources/mainClient.cpp
@@ -54,10 +54,11 @@ int main() {
 	
 	client.onData([&](const Message& message) {		
 		// std::cout << "Datas : - code : [" << message.code() << "] - size : " << message.size()/1000.0 << "KB \n";
-		unsigned long long t0 = message.timestamp();
+		static unsigned long long t0 = Timer::timestampMs();
 		unsigned long long t1 = Timer::timestampMs();
 		client.sendData(Message("Ping"));
 		std::cout << "Udp ping: " << t1 - t0 << " ms. \n";
+		t0 = t1;
 	});
 	
 	client.onInfo([&](const Message& message) {
