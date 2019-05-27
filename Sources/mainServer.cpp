32ff9de255d18f1691af0f279ca5d26effe06261
Saillou <chauvinremy@upyourprod.fr>
2019-05-27 11:02:38 +0900

    Automatic commit

--- Sources/mainServer.cpp
+++ Sources/mainServer.cpp
@@ -76,9 +76,9 @@ int main(int argc, char* argv[]) {
 		return std::cin.get();
 	}
 
-	std::string messageStr(30000, 'A');
+	std::string messageStr(60000, 'A');
 	
-	for(Timer timer; Globals::signalStatus != SIGINT; timer.wait(30)) {
+	for(Timer timer; Globals::signalStatus != SIGINT; timer.wait(20)) {
 		// Spam broadcast
 		for(auto& client: server.getClients()) {
 			if(client.connected) {
