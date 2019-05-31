69ac46bf77158cbcfadd674c26597da6db0b66d6
Saillou <chauvinremy@upyourprod.fr>
2019-05-31 17:46:37 +0900

    Automatic commit

--- Sources/Device/Device_impl_linux.hpp
+++ Sources/Device/Device_impl_linux.hpp
@@ -78,12 +78,9 @@ public:
 			goto failed;
 		
 		// Start	 
-		if(!_askFrame())
-			goto failed;
-		
 		if(!hvl::startCapture(_fd))
-			goto failed;
-		
+			return false;
+
 		// ----- Success -----
 		_open = true;
 		return true;		
@@ -126,6 +123,9 @@ public:
 	
 	bool grab() {
 		if(!_open)
+			return false;
+		
+		if(!_askFrame())
 			return false;
 		
 		struct v4l2_buffer buf = {0};
