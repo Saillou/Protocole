9d16859157acfa6bc6da4b625609e3eb76856e02
Saillou <chauvinremy@upyourprod.fr>
2019-06-03 05:11:34 +0900

    Automatic commit

--- Sources/mainServer.cpp
+++ Sources/mainServer.cpp
@@ -34,16 +34,16 @@ int main(int argc, char* argv[]) {
 	std::signal(SIGINT, sigintHandler);
 	
 	// - Device
-	// ServerDevice device0(Globals::PATH_0, 6666);
+	ServerDevice device0(Globals::PATH_0, 6666);
 	ServerDevice device1(Globals::PATH_1, 8888);
 
 	
 	// -------- Main loop --------  
-	// if(!device0.open(-1)) {
-		// std::cout << "Can't open device" << std::endl;
-		// std::cout << "Press a key to continue..." << std::endl;
-		// return std::cin.get();
-	// }
+	if(!device0.open(-1)) {
+		std::cout << "Can't open device" << std::endl;
+		std::cout << "Press a key to continue..." << std::endl;
+		return std::cin.get();
+	}
 	if(!device1.open(-1)) {
 		std::cout << "Can't open device" << std::endl;
 		std::cout << "Press a key to continue..." << std::endl;
@@ -59,18 +59,18 @@ int main(int argc, char* argv[]) {
 	
 	for(Timer timer; Globals::signalStatus != SIGINT; timer.wait(100)) {
 		// // ... Do other stuff ...
-		if(timer.elapsed_mus() > 10000000) {
-			std::cout << "Change format" << std::endl;
-			// device0.setFormat(fmtList[i].first, fmtList[i].second, Device::MJPG);
-			device1.setFormat(fmtList[i].first, fmtList[i].second, Device::MJPG);
+		// if(timer.elapsed_mus() > 10000000) {
+			// std::cout << "Change format" << std::endl;
+			// // device0.setFormat(fmtList[i].first, fmtList[i].second, Device::MJPG);
+			// device1.setFormat(fmtList[i].first, fmtList[i].second, Device::MJPG);
 			
-			i = (i + 1) % fmtList.size();
-			timer.beg();
-		}
+			// i = (i + 1) % fmtList.size();
+			// timer.beg();
+		// }
 	}
 	
 	// -- End
-	// device0.close();
+	device0.close();
 	device1.close();
 	
 	std::cout << "Clean exit" << std::endl;
