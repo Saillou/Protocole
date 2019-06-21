ce1c2be275b23db87ef1f10c2ea36f5826e81f10
Saillou <chauvinremy@upyourprod.fr>
2019-06-21 16:48:13 +0900

    Automatic commit

--- compile.sh
+++ compile.sh
@@ -3,7 +3,7 @@ git pull
 
 echo " ---- Compile ----"
 g++ -o Server -std=gnu++17 \
-Sources/mainServer.cpp  \
+Sources/mainServer.cpp Sources/GPIO/Gpio.cpp Sources/GPIO/ManagerGpio.cpp  \
 -I/opt/libjpeg-turbo/include  \
 -L/opt/libjpeg-turbo/lib64  \
 -lpthread -lturbojpeg -lopenh264
